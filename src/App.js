c6cd51d87eab86eef6ad629ec28e1c266a0559bb
malleswarchinta92 <malleswar.chinta@gmail.com>
2020-12-10 20:26:58 +0530

    axios to get prev and next list of pokemon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import PokemonList from './PokemonList';
+
 function App() {
+
+  const [pokemon, setPokemon] = useState([])
+  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextPage, setNextPage] = useState(null)
+  const [prevPage, setPrevPage] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+    setLoading(true)
+    axios.get(currentPage).then((resp)=>{
+      //for (let i = 0; i < 1000000000; i++) {}
+      setLoading(false)
+      setPokemon(resp.data.results)
+      setNextPage(resp.data.next)
+      setPrevPage(resp.data.previous)
+    })
+  }, [currentPage])
+
+  const goPrevious = () => {
+    setCurrentPage(prevPage)
+  }
+
+  const goNext = () =>{
+    setCurrentPage(nextPage)
+  }
+
   return (
-    null
-  );
+    <>
+    <button onClick={goPrevious} disabled={prevPage == null}>Previous</button>
+    <button onClick={goNext} disabled={nextPage == null}>Next</button>
+    {loading && <div>Loading</div>}
+    {!loading && <PokemonList pokemon={pokemon} />}
+    </>
+  )
 }
 
 export default App;
